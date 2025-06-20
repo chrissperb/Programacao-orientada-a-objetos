9d40bd291209f7cef6f2206cfc2d0bbdf1580713
chrisserb <chrissperb@gmail.com>
2025-06-20 15:22:34 -0300

    chore: organizacao do menu principal na classe main. chore: corrigindo o nome da classe ConversorStringParaInteger.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,127 @@
+import java.util.Scanner;
+
 public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+        int opcao;
+        do {
+            opcao = menuPrincipal();
+            if (opcao != 0) {
+                menuAtividade(opcao);
+            }
+        } while (opcao != 0);
 
+        System.out.println("Obrigado por avaliar esta atividade!");
+        scanner.close();
     }
 
+    private static int menuPrincipal() {
+        System.out.println("==============================================");
+        System.out.println("Universidade Unyleya");
+        System.out.println("Curso Analise e Desenvolvimento de Sistemas");
+        System.out.println("Disciplina: Programacao Orientada a Objetos");
+        System.out.println("Escolha uma opcao:");
+        System.out.println("1 - Atividade 1");
+        System.out.println("2 - Atividade 2");
+        System.out.println("3 - Atividade 3");
+        System.out.println("4 - Atividade Final");
+        System.out.println("0 - Sair");
+        System.out.println("==============================================");
+        System.out.println("Digite a opcao desejada:");
+
+        return lerOpcao();
+    }
+
+    private static void menuAtividade(int atividade) {
+        int opcao;
+        do {
+            System.out.println("==============================================");
+            System.out.println("Universidade Unyleya");
+            System.out.println("Curso Analise e Desenvolvimento de Sistemas");
+            System.out.println("Disciplina: Programacao Orientada a Objetos");
+            System.out.println("\nAtividade " + atividade + ":");
+
+            switch (atividade) {
+                case 1:
+                    System.out.println("1 - Animal");
+                    System.out.println("2 - Carro");
+                    System.out.println("3 - Circulo");
+                    System.out.println("4 - ContaBancaria");
+                    System.out.println("5 - Funcionario");
+                    System.out.println("6 - Livro");
+                    System.out.println("7 - Pessoa");
+                    System.out.println("8 - Produto");
+                    System.out.println("9 - Retangulo");
+                    System.out.println("10 - Triangulo");
+                    break;
+                case 2:
+                    System.out.println("1 - Autenticador");
+                    System.out.println("2 - Animais");
+                    System.out.println("3 - Calculadora");
+                    System.out.println("4 - ContaBancaria");
+                    System.out.println("5 - FormaGeometrica");
+                    System.out.println("6 - Veiculo");
+                    break;
+                case 3:
+                    System.out.println("1 - Calculadora");
+                    System.out.println("2 - ContaBancaria");
+                    System.out.println("3 - ConversorStrinngParaInteger");
+                    System.out.println("4 - ConversorTemperatura");
+                    System.out.println("5 - DivisaoInteira");
+                    System.out.println("6 - DivisaoPorZero");
+                    System.out.println("7 - LeitorDeArquivo");
+                    System.out.println("8 - SaldoInsuficiente");
+                    System.out.println("9 - ValidadorSenha");
+                    break;
+                case 4:
+                    System.out.println("1 - Projeto Final");
+                    break;
+            }
+            System.out.println("0 - Voltar ao menu principal");
+            System.out.println("==============================================");
+            System.out.println("Digite a opcao desejada:");
+
+            opcao = lerOpcao();
+            if (opcao != 0) {
+                executarClasse(atividade, opcao);
+            }
+        } while (opcao != 0);
+    }
+
+    private static void executarClasse(int atividade, int opcao) {
+        System.out.println("\nExecutando Atividade " + atividade + " - Opcao " + opcao);
+
+        switch (atividade) {
+            case 1:
+                switch (opcao) {
+                    case 1:
+                        // Exemplo: new atividade1.Animal().executar();
+                        System.out.println("Executando classe Animal...");
+                        break;
+                    case 2:
+                        // Exemplo: new atividade1.Carro().executar();
+                        System.out.println("Executando classe Carro...");
+                        break;
+                    case 3:
+                        // Exemplo: new atividade1.Circulo().executar();
+                        System.out.println("Executando classe Circulo...");
+                        break;
+                }
+                break;
+        }
+
+        System.out.println("\nPressione ENTER para continuar...");
+        scanner.nextLine();
+        scanner.nextLine();
+    }
+
+    private static int lerOpcao() {
+        try {
+            return Integer.parseInt(scanner.nextLine());
+        } catch (NumberFormatException e) {
+            System.out.println("Opcao invalida! Digite um numero.");
+            return -1;
+        }
+    }
 }
